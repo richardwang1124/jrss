867443e8bef71c98789397fabe79227a7a76ddba
Jude Bedessem <judejb2000@gmail.com>
2019-04-07 07:09:42 -0400

    servo_stuff w/ gui

--- arduino_stuff/sound_sensor/sound_sensor.ino
+++ arduino_stuff/sound_sensor/sound_sensor.ino
@@ -1,24 +1,30 @@
-#define SOUND_PIN A0
+#include <Servo.h>
 
-int bdd = 9600;
-int sound_pin=11;
-unsigned int list1[200];
+Servo myservo;
+
+int pin = 11;
+int down = 1;
+int up = 7;
+int test=0;
+
 void setup() {
-  Serial.begin(bdd);
-  int start = millis();
-  //pinMode(A0, INPUT);
-  for(int i=0; i<200 ; i++){
-     list1[i]=analogRead(A0);
-     //delay(10);
-     //Serial.println(analogRead(A0));
-  }
-  delay(1000);
-  //Serial.println("OUT");
-  Serial.print(millis()-start);
+  Serial.begin(9600);
+  myservo.attach(pin);
+
+  while(Serial.read() == -1){}
 }
 
 void loop() {
-  Serial.println("Done");
-  delay(1000);
-
+  if(Serial.read() != -1){
+    setup();
+  }
+  for(int i = 0;i<180;i++){
+   myservo.write(i);
+   delay(down);
+  }
+  for(int i = 180;i>0;i--){
+   myservo.write(i);
+   delay(up);
+  }
+  
 }
