955f921da478a01f67497636921052a8caa658ee
Shihao Cao <shihaocao@gmail.com>
2019-04-06 23:01:01 -0500

    220

--- jude1.py
+++ jude1.py
@@ -1,13 +1,17 @@
 import pyaudio
 import wave
+import sys
 
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 32000 # 44.1kHz sampling rate
 chunk = 4096 # 2^12 samples for buffer
-record_secs = 3 # seconds to record
+record_secs = 220 # seconds to record
 dev_index = 3 # device index found by p.get_device_info_by_index(ii)
-wav_output_filename = "J1.wav" # name of .wav file
+
+#print(sys.argv)
+fn = sys.argv[1]+".wav"
+wav_output_filename = fn # name of .wav file
 
 audio = pyaudio.PyAudio() # create pyaudio instantiation
 
@@ -16,6 +20,8 @@ stream = audio.open(format = form_1,rate = samp_rate,channels = chans, \
                     input_device_index = dev_index,input = True, \
                     frames_per_buffer=chunk)
 print("recording")
+print(sys.argv)
+
 frames = []
 
 # loop through stream and append audio chunks to frame array
@@ -37,3 +43,7 @@ wavefile.setsampwidth(audio.get_sample_size(form_1))
 wavefile.setframerate(samp_rate)
 wavefile.writeframes(b''.join(frames))
 wavefile.close()
+
+print("DONE")
+print("FILENAME: "+fn)
+
