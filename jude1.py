45479d239190b00289eb3c641359bfd2f9c034a7
Shihao Cao <shihaocao@gmail.com>
2019-04-06 23:27:38 -0500

    chunk

--- jude1.py
+++ jude1.py
@@ -5,7 +5,7 @@ import sys
 form_1 = pyaudio.paInt16 # 16-bit resolution
 chans = 1 # 1 channel
 samp_rate = 32000 # 44.1kHz sampling rate
-chunk = 4096 # 2^12 samples for buffer
+chunk = int(sys.argv[3]) # 2^12 samples for buffer
 record_secs = int(sys.argv[2]) # seconds to record
 dev_index = 3 # device index found by p.get_device_info_by_index(ii)
 
