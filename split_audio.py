03e43f8dfdd7a79487737c38230d7a2afc613159
Jude Bedessem <judejb2000@gmail.com>
2019-04-07 04:03:02 -0400

    the splices are done

--- split_audio.py
+++ split_audio.py
@@ -1,11 +1,11 @@
 import ffmpeg
 from pydub import AudioSegment
 
-filename="220.wav"
-size=220
-length=2000
+filename="cc1.wav"
+size=216
+length=1000
 
-for i in range(0, int(size/2)):
+for i in range(1, int(size/2)):
     newAudio = AudioSegment.from_wav(filename)
-    newAudio = newAudio[length*i:length*i+2000]
-    newAudio.export("splices/A1/clip%s.wav" % i)
+    newAudio = newAudio[length*i-length/2:length*i+length-length/2]
+    newAudio.export("splices/C2/clip%s.wav" % i)
